d80294047c0974e57af336e0f05ed1c82eaad1b4
Mateusz Kaduk <mateusz.kaduk@gmail.com>
2024-08-01 09:29:28 +0200

    src/SequenceTokenizers.jl: Optimize from 400ns to 200ns by reducing allocations and improving type stability.

--- src/SequenceTokenizers.jl
+++ src/SequenceTokenizers.jl
@@ -4,18 +4,6 @@ module SequenceTokenizers
 
     export SequenceTokenizer
 
-    """
-        SequenceTokenizer(alphabet::Vector{T}, unksym::T) where T
-
-    Tokenizes character sequences.
-
-    # Arguments
-    - `alphabet::Vector{T}`: A vector of symbols to be tokenized.
-    - `unksym::T`: The symbol for unknown tokens.
-
-    # Returns
-    - `SequenceTokenizer{T}`: A `SequenceTokenizer` object.
-    """
     struct SequenceTokenizer{T, V <: AbstractVector{T}}
         alphabet::V
         lookup::Dict{T, Int32}
@@ -23,48 +11,48 @@ module SequenceTokenizers
         unkidx::Int32
 
         function SequenceTokenizer(alphabet::V, unksym::T) where {T, V <: AbstractVector{T}}
-            if !(unksym ∈ alphabet)
+            if !(unksym in alphabet)
                 alphabet = vcat(unksym, alphabet)
                 unkidx = Int32(1)
             else
-                unkidx = findfirst(isequal(unksym), alphabet)
+                unkidx = Int32(findfirst(isequal(unksym), alphabet))
             end
-            lookup = Dict(x => Int32(idx) for (idx, x) in enumerate(alphabet))
+            lookup = Dict{T, Int32}(x => Int32(idx) for (idx, x) in enumerate(alphabet))
             new{T, V}(alphabet, lookup, unksym, unkidx)
         end
     end
 
     Base.length(tokenizer::SequenceTokenizer) = length(tokenizer.alphabet)
 
-    function Base.show(io::IO, tokenizer::SequenceTokenizer{T}) where T
-        print(io, "SequenceTokenizer{$(T)}(length(alphabet)=$(length(tokenizer)), unksym=$(tokenizer.unksym))")
+    Base.show(io::IO, tokenizer::SequenceTokenizer{T}) where T =
+        print(io, "SequenceTokenizer{$T}(length(alphabet)=$(length(tokenizer)), unksym=$(tokenizer.unksym))")
+
+    @inline (tokenizer::SequenceTokenizer{T})(token::T) where T =
+        get(tokenizer.lookup, token, tokenizer.unkidx)
+
+    @inline (tokenizer::SequenceTokenizer)(idx::Integer) = tokenizer.alphabet[idx]
+
+    function (tokenizer::SequenceTokenizer{T})(x::AbstractArray) where T
+        return map(tokenizer, x)
     end
 
-    function (tokenizer::SequenceTokenizer{T})(token::T) where T
-        haskey(tokenizer.lookup, token) ? tokenizer.lookup[token] : tokenizer.unkidx
-    end
-
-    function (tokenizer::SequenceTokenizer)(idx::Integer)
-        tokenizer.alphabet[idx]
-    end
-
-    function (tokenizer::SequenceTokenizer{T})(x::A) where {T, A <: AbstractArray}
-        map(i -> tokenizer(i), x)
-    end
-
-    function (tokenizer::SequenceTokenizer{T})(batch::A) where {T, A <: AbstractVector{<:AbstractVector{T}}}
-        lengths = map(length, batch)
-        max_length = maximum(lengths)
-        indices = fill(tokenizer.unkidx, max_length, length(batch))
-        for j in eachindex(batch)
-            local seq = batch[j]
+    function (tokenizer::SequenceTokenizer{T})(batch::AbstractVector{<:AbstractVector{T}}) where T
+        max_length = maximum(length, batch)
+        indices = Matrix{Int32}(undef, max_length, length(batch))
+        
+        @inbounds for (j, seq) in enumerate(batch)
             for i in eachindex(seq)
-                @inbounds indices[i, j] = tokenizer(seq[i])
+                indices[i, j] = tokenizer(seq[i])
+            end
+            for i in (length(seq) + 1):max_length
+                indices[i, j] = tokenizer.unkidx
             end
         end
-        indices
+        
+        return indices
     end
 
     Functors.@functor SequenceTokenizer
-    Optimisers.trainable(tokenizer::SequenceTokenizer) = (;)
+    Optimisers.trainable(tokenizer::SequenceTokenizer) = NamedTuple()
+
 end
